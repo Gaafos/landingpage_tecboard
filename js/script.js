5ec0c23c1246f477a301898586551d591ea5450e
Gaafos <gaafos@gmail.com>
2026-03-15 21:44:54 -0300

    ajuster para github pages

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ function trocarModo(){
     let logo = document.getElementById('logo');
     let background = document.querySelector('body');
     if(botaoModo.src.includes('whitemode.png')){
-        botaoModo.src = '/img/darkmode.png';
-        logo.src = '/img/logo2.png';
+        botaoModo.src = 'img/darkmode.png';
+        logo.src = 'img/logo2.png';
         document.body.classList.add('whiteMode');
     }else{
-        botaoModo.src = '/img/whitemode.png';
+        botaoModo.src = 'img/whitemode.png';
         document.body.classList.remove('whiteMode');
-        logo.src = '/img/logo.png';
+        logo.src = 'img/logo.png';
     }
 
 }
