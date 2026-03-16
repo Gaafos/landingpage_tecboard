3061286872306a1d5e7af688e95c89d4025f4567
Gaafos <gaafos@gmail.com>
2026-03-15 22:09:05 -0300

    ajustes na responsividade e detalhes

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ function trocarModo(){
     let botaoModo = document.getElementById('trocaDeModo');
     let logo = document.getElementById('logo');
     let background = document.querySelector('body');
+    let favicon = document.getElementById('favicon');
+
     if(botaoModo.src.includes('whitemode.png')){
         botaoModo.src = 'img/darkmode.png';
         logo.src = 'img/logo2.png';
         document.body.classList.add('whiteMode');
+        favicon.href = 'img/favicoBranco.png';
     }else{
         botaoModo.src = 'img/whitemode.png';
         document.body.classList.remove('whiteMode');
         logo.src = 'img/logo.png';
+        favicon.href = 'img/faviconRoxo.png';
     }
 
 }
